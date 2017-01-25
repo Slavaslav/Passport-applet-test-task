9dc697746255f2403839ff3422e47dc8bf53d86e
awecea <awecea@gmail.com>
2017-01-25 12:59:05 +0200

    refactor

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -24,46 +24,14 @@ public class Main extends Applet {
     private void openNewJFrameInputPassportData() {
         final JFrame frame = new JFrame("Input Passport Data");
 
-        ArrayList<Label> labels = new ArrayList<>();
-        labels.add(new Label("Input passportNo:"));
-        labels.add(new Label("Input surname:"));
-        labels.add(new Label("Input given names:"));
-        labels.add(new Label("Input patronymic:"));
-        labels.add(new Label("Input date birth:"));
-        labels.add(new Label("Input place birth:"));
-        labels.add(new Label("Input authority:"));
-        labels.add(new Label("Input date of issue:"));
-        final Label errorEmptyFields = new Label("Error! One or more fields are not filled!");
-        errorEmptyFields.setVisible(false);
-        errorEmptyFields.setForeground(Color.RED);
-        labels.add(errorEmptyFields);
-
-        final int columnsTextField = 40;
-        final ArrayList<TextField> textFields = new ArrayList<>();
-        textFields.add(new TextField(columnsTextField));
-        textFields.add(new TextField(columnsTextField));
-        textFields.add(new TextField(columnsTextField));
-        textFields.add(new TextField(columnsTextField));
-        textFields.add(new TextField(columnsTextField));
-        textFields.add(new TextField(columnsTextField));
-        textFields.add(new TextField(columnsTextField));
-        textFields.add(new TextField(columnsTextField));
+        final ArrayList<Label> labels = initializeLabels();
+        final ArrayList<TextField> textFields = initializeTextFields();
 
         Button okButton = new Button("Ok");
         okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                for (TextField field : textFields) {
-                    if (field.getText().isEmpty()) {
-                        errorEmptyFields.setVisible(true);
-                        frame.pack();
-                        return;
-                    }
-                }
-                if (errorEmptyFields.isVisible()) {
-                    errorEmptyFields.setVisible(false);
-                    frame.pack();
-                }
+                handleOnClickOkButton(labels, textFields, frame);
             }
         });
         Button closeButton = new Button("Close");
@@ -74,6 +42,62 @@ public class Main extends Applet {
             }
         });
 
+        JPanel jPanelRoot = initializeJPanels(labels, textFields, new Button[]{okButton, closeButton});
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(jPanelRoot);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private ArrayList<Label> initializeLabels() {
+        ArrayList<Label> labels = new ArrayList<>();
+        labels.add(new Label("Input passportNo:"));
+        labels.add(new Label("Input surname:"));
+        labels.add(new Label("Input given names:"));
+        labels.add(new Label("Input patronymic:"));
+        labels.add(new Label("Input date birth:"));
+        labels.add(new Label("Input place birth:"));
+        labels.add(new Label("Input authority:"));
+        labels.add(new Label("Input date of issue:"));
+        // label error must be last element in array
+        final Label errorEmptyFields = new Label("Error! One or more fields are not filled!");
+        errorEmptyFields.setVisible(false);
+        errorEmptyFields.setForeground(Color.RED);
+        labels.add(errorEmptyFields);
+        return labels;
+    }
+
+    private ArrayList<TextField> initializeTextFields() {
+        final int columnsTextField = 40;
+        ArrayList<TextField> textFields = new ArrayList<>();
+        textFields.add(new TextField(columnsTextField));
+        textFields.add(new TextField(columnsTextField));
+        textFields.add(new TextField(columnsTextField));
+        textFields.add(new TextField(columnsTextField));
+        textFields.add(new TextField(columnsTextField));
+        textFields.add(new TextField(columnsTextField));
+        textFields.add(new TextField(columnsTextField));
+        textFields.add(new TextField(columnsTextField));
+        return textFields;
+    }
+
+    private void handleOnClickOkButton(ArrayList<Label> labels, ArrayList<TextField> textFields, JFrame frame) {
+        Label errorEmptyLabel = labels.get(labels.size() - 1);
+        for (TextField field : textFields) {
+            if (field.getText().isEmpty()) {
+                errorEmptyLabel.setVisible(true);
+                frame.pack();
+                return;
+            }
+        }
+        if (errorEmptyLabel.isVisible()) {
+            errorEmptyLabel.setVisible(false);
+            frame.pack();
+        }
+    }
+
+    private JPanel initializeJPanels(ArrayList<Label> labels, ArrayList<TextField> textFields, Button[] buttons) {
         JPanel jPanelLabelAndTextFields = new JPanel();
         jPanelLabelAndTextFields.setLayout(new BoxLayout(jPanelLabelAndTextFields, BoxLayout.Y_AXIS));
 
@@ -89,8 +113,10 @@ public class Main extends Applet {
 
         JPanel jPanelButtons = new JPanel();
         jPanelButtons.setLayout(new BoxLayout(jPanelButtons, BoxLayout.X_AXIS));
-        jPanelButtons.add(okButton);
-        jPanelButtons.add(closeButton);
+        for (Button button : buttons) {
+            jPanelButtons.add(button);
+
+        }
 
         JPanel jPanelRoot = new JPanel();
         jPanelRoot.setLayout(new BoxLayout(jPanelRoot, BoxLayout.Y_AXIS));
@@ -98,9 +124,6 @@ public class Main extends Applet {
         jPanelRoot.add(jPanelLabelAndTextFields);
         jPanelRoot.add(jPanelButtons);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(jPanelRoot);
-        frame.pack();
-        frame.setVisible(true);
+        return jPanelRoot;
     }
 }
