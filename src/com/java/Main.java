dff7c9b50d1f4a5b19e8b4b96bd6fca769d818d7
awecea <awecea@gmail.com>
2017-01-25 11:55:25 +0200

    add textFields add buttons

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -21,9 +21,44 @@ public class Main extends Applet {
     }
 
     private void openNewJFrameInputPassportData() {
+        final int columnsTextField = 30;
+        TextField passportNoTextField = new TextField(columnsTextField);
+        TextField surnameTextField = new TextField(columnsTextField);
+        TextField givenNamesTextField = new TextField(columnsTextField);
+        TextField patronymicTextField = new TextField(columnsTextField);
+        TextField dateBirthTextField = new TextField(columnsTextField);
+        TextField placeBirthTextField = new TextField(columnsTextField);
+        TextField authorityTextField = new TextField(columnsTextField);
+        TextField dateOfIssueTextField = new TextField(columnsTextField);
+        Button okButton = new Button("Ok");
+        Button closeButton = new Button("Close");
+
         JFrame frame = new JFrame("Input Passport Data");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new Button("Test"), BorderLayout.CENTER);
+
+        JPanel jPanelRoot = new JPanel();
+        jPanelRoot.setLayout(new BoxLayout(jPanelRoot, BoxLayout.Y_AXIS));
+
+        JPanel jPanelTextFields = new JPanel();
+        jPanelTextFields.setLayout(new BoxLayout(jPanelTextFields, BoxLayout.Y_AXIS));
+        jPanelTextFields.add(passportNoTextField, BorderLayout.PAGE_START);
+        jPanelTextFields.add(surnameTextField, BorderLayout.AFTER_LAST_LINE);
+        jPanelTextFields.add(givenNamesTextField, BorderLayout.AFTER_LAST_LINE);
+        jPanelTextFields.add(patronymicTextField, BorderLayout.AFTER_LAST_LINE);
+        jPanelTextFields.add(dateBirthTextField, BorderLayout.AFTER_LAST_LINE);
+        jPanelTextFields.add(placeBirthTextField, BorderLayout.AFTER_LAST_LINE);
+        jPanelTextFields.add(authorityTextField, BorderLayout.AFTER_LAST_LINE);
+        jPanelTextFields.add(dateOfIssueTextField, BorderLayout.AFTER_LAST_LINE);
+
+        JPanel jPanelButtons = new JPanel();
+        jPanelButtons.setLayout(new BoxLayout(jPanelButtons, BoxLayout.X_AXIS));
+        jPanelButtons.add(okButton);
+        jPanelButtons.add(closeButton);
+
+        jPanelRoot.add(jPanelTextFields);
+        jPanelRoot.add(jPanelButtons);
+
+        frame.add(jPanelRoot);
         frame.pack();
         frame.setVisible(true);
     }
