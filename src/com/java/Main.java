8c553675b50595648911ccc798f0b23c13ce61cd
awecea <awecea@gmail.com>
2017-01-25 12:14:04 +0200

    add text labels add buttons listener

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -21,7 +21,18 @@ public class Main extends Applet {
     }
 
     private void openNewJFrameInputPassportData() {
-        final int columnsTextField = 30;
+        final JFrame frame = new JFrame("Input Passport Data");
+
+        Label passportNoLabel = new Label("Input passportNo:");
+        Label surnameLabel = new Label("Input surname:");
+        Label givenNamesLabel = new Label("Input given names:");
+        Label patronymicLabel = new Label("Input patronymic:");
+        Label dateBirthLabel = new Label("Input date birth:");
+        Label placeBirthLabel = new Label("Input place birth:");
+        Label authorityLabel = new Label("Input authority:");
+        Label dateOfIssueLabel = new Label("Input date of issue:");
+
+        final int columnsTextField = 40;
         TextField passportNoTextField = new TextField(columnsTextField);
         TextField surnameTextField = new TextField(columnsTextField);
         TextField givenNamesTextField = new TextField(columnsTextField);
@@ -31,33 +42,51 @@ public class Main extends Applet {
         TextField authorityTextField = new TextField(columnsTextField);
         TextField dateOfIssueTextField = new TextField(columnsTextField);
         Button okButton = new Button("Ok");
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("click ok");
+            }
+        });
         Button closeButton = new Button("Close");
-
-        JFrame frame = new JFrame("Input Passport Data");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel jPanelRoot = new JPanel();
-        jPanelRoot.setLayout(new BoxLayout(jPanelRoot, BoxLayout.Y_AXIS));
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
 
         JPanel jPanelTextFields = new JPanel();
         jPanelTextFields.setLayout(new BoxLayout(jPanelTextFields, BoxLayout.Y_AXIS));
-        jPanelTextFields.add(passportNoTextField, BorderLayout.PAGE_START);
-        jPanelTextFields.add(surnameTextField, BorderLayout.AFTER_LAST_LINE);
-        jPanelTextFields.add(givenNamesTextField, BorderLayout.AFTER_LAST_LINE);
-        jPanelTextFields.add(patronymicTextField, BorderLayout.AFTER_LAST_LINE);
-        jPanelTextFields.add(dateBirthTextField, BorderLayout.AFTER_LAST_LINE);
-        jPanelTextFields.add(placeBirthTextField, BorderLayout.AFTER_LAST_LINE);
-        jPanelTextFields.add(authorityTextField, BorderLayout.AFTER_LAST_LINE);
-        jPanelTextFields.add(dateOfIssueTextField, BorderLayout.AFTER_LAST_LINE);
+        jPanelTextFields.add(passportNoLabel);
+        jPanelTextFields.add(passportNoTextField);
+        jPanelTextFields.add(surnameLabel);
+        jPanelTextFields.add(surnameTextField);
+        jPanelTextFields.add(givenNamesLabel);
+        jPanelTextFields.add(givenNamesTextField);
+        jPanelTextFields.add(patronymicLabel);
+        jPanelTextFields.add(patronymicTextField);
+        jPanelTextFields.add(dateBirthLabel);
+        jPanelTextFields.add(dateBirthTextField);
+        jPanelTextFields.add(placeBirthLabel);
+        jPanelTextFields.add(placeBirthTextField);
+        jPanelTextFields.add(authorityLabel);
+        jPanelTextFields.add(authorityTextField);
+        jPanelTextFields.add(dateOfIssueLabel);
+        jPanelTextFields.add(dateOfIssueTextField);
 
         JPanel jPanelButtons = new JPanel();
         jPanelButtons.setLayout(new BoxLayout(jPanelButtons, BoxLayout.X_AXIS));
         jPanelButtons.add(okButton);
         jPanelButtons.add(closeButton);
 
+        JPanel jPanelRoot = new JPanel();
+        jPanelRoot.setLayout(new BoxLayout(jPanelRoot, BoxLayout.Y_AXIS));
+        jPanelRoot.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         jPanelRoot.add(jPanelTextFields);
         jPanelRoot.add(jPanelButtons);
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(jPanelRoot);
         frame.pack();
         frame.setVisible(true);
